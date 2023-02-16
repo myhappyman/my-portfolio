831ba916815e778c81ebed922d05f40cd024e250
ParkShinwoo <myhappyman@naver.com>
2023-02-16 22:22:35 +0900

    메인 포트폴리오 기본페이지 시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+const Area = styled.div`
+  width: 100%;
+  font-size: 16rem;
+  /* display: flex;
+  justify-content: space-between; */
+`;
+const CArea = styled.span``;
+const Character = styled.a<{ height: number }>`
+  display: inline-block;
+  width: calc(100% / 13);
+  padding-top: 10px;
+  /* text-align: center; */
+  /* letter-spacing: 8.4rem; */
+  line-height: 50%;
+  transform: scale(1, ${(props) => props.height});
+  transform-origin: 0 0;
+  /* outline: 1px solid red; */
+  font-family: "Anton", sans-serif;
+`;
 
 function App() {
+  const [height, setHeight] = useState<number[]>([]);
+  useEffect(() => {
+    for (let i = 0; i < 13; i++) {
+      const number = Math.random() * (3.6 - 1.8) + 1.8;
+      setHeight((prev) => [...prev, number]);
+    }
+  }, []);
+  console.log(height);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Area>
+        <Character height={height[0]}>S</Character>
+        <Character height={height[1]}>H</Character>
+        <Character height={height[2]}>I</Character>
+        <Character height={height[3]}>N</Character>
+        <Character height={height[4]}>P</Character>
+        <Character height={height[5]}>O</Character>
+        <Character height={height[6]}>R</Character>
+        <Character height={height[7]}>T</Character>
+        <Character height={height[8]}>F</Character>
+        <Character height={height[9]}>O</Character>
+        <Character height={height[10]}>L</Character>
+        <Character height={height[11]}>I</Character>
+        <Character height={height[12]}>O</Character>
+      </Area>
+    </Wrapper>
   );
 }
 
