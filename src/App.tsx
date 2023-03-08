1c8502fa34bfdda7f6edc805bbdbdc63e26b4a7c
ParkShinwoo <myhappyman@naver.com>
2023-03-08 23:17:35 +0900

    firebase 세팅 테스트 진행 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import styled, { ThemeProvider } from "styled-components";
 import { themeMode, themeSelectIsOpen } from "./atom";
@@ -11,6 +12,8 @@ import Projects from "./components/Section/Projects";
 import SecondSection from "./components/Section/SecondSection";
 import Skills from "./components/Section/Skills";
 import Works from "./components/Section/Works";
+import { db } from "./firebase-config";
+
 import GlobalStyle from "./GlobalStyle";
 import { marsTheme, moonTheme } from "./theme";
 
@@ -18,6 +21,21 @@ function App() {
   const selectTheme = useRecoilValue(themeMode);
   const [isOpen, setIsOpen] = useRecoilState(themeSelectIsOpen); // 바디 클릭하면 테마모드 셀렉터 닫히도록
   const toggleThemeSelectIsOpen = () => (isOpen ? setIsOpen(false) : null);
+
+  // console.log(getReadDoc("contacts", "blog"));
+
+  const usersCollectionRef = collection(db, "users");
+  // 시작될때 한번만 실행
+  useEffect(() => {
+    // 비동기로 데이터 받을준비
+    const getUsers = async () => {
+      // getDocs로 컬렉션안에 데이터 가져오기
+      const data = await getDocs(usersCollectionRef);
+      console.log(data);
+    };
+
+    getUsers();
+  }, []);
 
   return (
     <ThemeProvider theme={selectTheme === "mars" ? marsTheme : moonTheme}>
---
+++ src/FireBaseRead.ts
@@ -0,0 +1,8 @@
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  getFirestore,
+} from "firebase/firestore";
+import { app, db } from "./firebase-config";
